6f82223e3e4a72d644030e95c71ce73987229011
xelpmocShubham <shubham.mailme@gmail.com>
2017-05-03 20:19:06 +0530

    Removed

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express        = require('express');
 const app            = express();
-const mongoose       = require('mongoose');
 const bodyParser     = require('body-parser');
 const path = require('path');
 
 
-// config files by Ashu
-var db = require('./config/db');
-var port = process.env.PORT || 3000; // set our port
+// config files
+//here i changed ok
+
+var port = process.env.PORT || 8000; // set our port
 
 // mongoose.connect(db.url); // connect to our mongoDB database (commented out after you enter in your own credentials)
 
-// get all data/stuff of the body (POST) parameters
+// get all data/stuff of the body (db) parameters
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
@@ -24,10 +24,13 @@ app.get('/', function (req, res) {
 });
 
 
+
+
+app.use('/',require('./routes/index.js'));
 // routes ==================================================
 //require('./app/routes')(app); // pass our application into our routes
 
 // start app ===============================================
 app.listen(port);
-console.log('Magic happens on port, which is' + port);
+console.log('Magic happens on port, which is ' + port); 
 exports = module.exports = app;
