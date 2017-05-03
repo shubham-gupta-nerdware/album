ec17d6d9c0b6b113516c183798272b3143d6ab33
Manish <mkjha9555@gmail.com>
2017-05-03 18:26:14 +0530

    Commit message

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const mongoose       = require('mongoose');
 const bodyParser     = require('body-parser');
 const path = require('path');
 
-	
-// config files
+
+// config files by manish
 var db = require('./config/db');
 var port = process.env.PORT || 3000; // set our port
 
 // mongoose.connect(db.url); // connect to our mongoDB database (commented out after you enter in your own credentials)
 
 // get all data/stuff of the body (POST) parameters
-app.use(bodyParser.json()); // parse application/json 
+app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 
@@ -28,6 +28,6 @@ app.get('/', function (req, res) {
 //require('./app/routes')(app); // pass our application into our routes
 
 // start app ===============================================
-app.listen(port);	
-console.log('Magic happens on port, which is' + port); 
-exports = module.exports = app; 			
+app.listen(port);
+console.log('Magic happens on port, which is' + port);
+exports = module.exports = app;
