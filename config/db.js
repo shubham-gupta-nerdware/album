dd2724a318a47b51c9c50333343ff071bf1453c3
xelpmocShubham <shubham.mailme@gmail.com>
2017-05-03 20:02:23 +0530

    Db Connection Details

--- config/db.js
+++ config/db.js
@@ -5,3 +5,33 @@
  */
 
 
+
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var MongoClient = require('mongodb').MongoClient;
+
+// Connect to the db
+var url ="mongodb://localhost:27017/db_homehapp_mongo";
+//MongoClient.connect("mongodb://localhost:27017/admin", function(err, db) {
+//
+//});
+/*
+ * db.collection.update(
+   {username:"Bob"},
+   {$set:{'longitude': '58.3', 'latitude': '0.3'}},
+   { upsert: true}
+)
+ */
+/*
+ * db.collection.ensureIndex( { "username": 1 }, { unique: true } )
+ * 
+ */
+module.exports= {MongoClient: MongoClient,
+                url:url};
+   
