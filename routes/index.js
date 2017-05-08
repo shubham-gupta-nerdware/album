94af5360a55a527e83830199cefec08daa584e39
xelpmocShubham <rajesh_bankar80@yahoo.com>
2017-05-08 12:05:22 +0530

    Photos upload done

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,8 @@ router.get('/allstories/:lt/:st', storyControllerObj.getStoryData);//version
 router.get('/storyDetails/:storyId', storyControllerObj.getStoryDetails);//version
 
 
+var uploadPhotoVideoControllerObj =  require('../controllers/uploadPhotoVideoController.js');
+router.post('/photosUpload/:storyId/:ptype', uploadPhotoVideoControllerObj.photosUpload);//version
+
+
 module.exports = router;
